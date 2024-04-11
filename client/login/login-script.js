cb327d6546c2e8112a0add2426eeeab9eba65eeb
yuchiro <czarniknatalia16@gmail.com>
2024-04-11 20:25:49 +0200

    koniec robienia strony logowania :3
    cool

---
+++ client/login/login-script.js
@@ -0,0 +1,115 @@
+//Login
+const loginButton = document.querySelector("#login-submit-button");
+const showPasswordIcon = document.querySelector("#password-eye-icon");
+const usernameLoginInput = document.querySelector("#login-username-input");
+const passwordLoginInput = document.querySelector("#login-password-input");
+
+showPasswordIcon.addEventListener('click', ()=>{
+    if(passwordLoginInput.type == "password") {
+        passwordLoginInput.setAttribute('type', "text");
+        showPasswordIcon.setAttribute('name', "eye-outline");
+    }else {
+        passwordLoginInput.setAttribute('type', "password");
+        showPasswordIcon.setAttribute('name', "eye-off-outline");
+    }
+});
+
+usernameLoginInput.addEventListener('input', ()=>{
+    let nameTab = [];
+    nameTab = usernameLoginInput.value.split("");
+    if(nameTab.length>20){
+        nameTab.pop();
+        nameTab = nameTab.join("");
+        usernameLoginInput.value = nameTab;
+    }
+});
+
+loginButton.addEventListener('click', (event)=>{
+    event.preventDefault();
+});
+
+//Register
+const registerButton = document.querySelector("#register-submit-button");
+const usernameRegisterInput = document.querySelector("#register-username-input");
+const passwordRegisterInput = document.querySelector("#register-password-input");
+const repeatPasswordInput = document.querySelector("#register-repeat-password-input");
+const repeatedPasswordDiv = document.querySelector(".repeat-password-div");
+const classChangeArray = document.querySelectorAll(".register-change-color");
+
+const checkPassword = ()=>{
+    let originalPassword = passwordRegisterInput.value.split("");
+    let repeatedPassword = repeatPasswordInput.value.split("");
+    let matching = false;
+    for(let i = 0; i <= originalPassword.length; i++){
+        if(originalPassword.length!=repeatedPassword.length){
+            matching = false;
+            break;
+        }
+        if(originalPassword[i]!=repeatedPassword[i]){
+            matching = false;
+            break;
+        }
+        matching = true;
+    }
+    if(matching == false){
+        if(repeatedPasswordDiv.nextElementSibling.nodeName!=='P'){
+            let errorMessageParagraph = document.createElement('p');
+            errorMessageParagraph.textContent = "Passwords should be the same!";
+            errorMessageParagraph.setAttribute('id', "password-error");
+            repeatedPasswordDiv.insertAdjacentElement("afterend", errorMessageParagraph);
+        }
+        
+        classChangeArray.forEach((item)=>{
+            if(item.classList.contains("matching-password")) item.classList.remove("matching-password");
+            item.classList.add("not-matching-password");
+        });
+    }else{
+        const deleteElement = document.querySelector("#password-error");
+        deleteElement.remove();
+        
+        classChangeArray.forEach((item)=>{
+            if(item.classList.contains("not-matching-password")) item.classList.remove("not-matching-password");
+            item.classList.add("matching-password");
+        });
+    }
+}
+
+usernameRegisterInput.addEventListener('input', ()=>{
+    let nameTab = [];
+    nameTab = usernameRegisterInput.value.split("");
+    if(nameTab.length>20){
+        nameTab.pop();
+        nameTab = nameTab.join("");
+        usernameRegisterInput.value = nameTab;
+    }
+});
+
+repeatPasswordInput.addEventListener('input', ()=>{
+    if((passwordRegisterInput.value.length!==0)&&(repeatPasswordInput.value.length!==0)){
+        checkPassword();
+    }else {
+        classChangeArray.forEach((item)=>{
+            item.classList.remove("matching-password");
+            item.classList.remove("not-matching-password");
+            const deleteElement = document.querySelector("#password-error");
+            deleteElement.remove();
+        });
+    }
+});
+
+passwordRegisterInput.addEventListener('input', ()=>{
+    if((repeatPasswordInput.value.length!==0)&&(passwordRegisterInput.value.length!==0)){
+        checkPassword();
+    }else {
+        classChangeArray.forEach((item)=>{
+            item.classList.remove("matching-password");
+            item.classList.remove("not-matching-password");
+            const deleteElement = document.querySelector("#password-error");
+            deleteElement.remove();
+        });
+    }
+});
+
+registerButton.addEventListener('click', (event)=>{
+    event.preventDefault();
+});
