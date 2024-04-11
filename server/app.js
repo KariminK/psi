b68f9d55220f2f08f94c15f33d6dee2e72a429bf
KariminK <103779342+KariminK@users.noreply.github.com>
2024-04-11 18:07:34 +0200

    added express, mysql and cors modules

--- server/app.js
+++ server/app.js
@@ -1,4 +1,33 @@
-const http = require("http");
-http.listen(3000, "localhost", () => {
-  console.log("SERVER STARTED! PORT: 3000");
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
+
+const DATABASE_CONN = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "psi",
+});
+
+app.use(cors({}));
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+  DATABASE_CONN.connect((err) => {
+    if (err) {
+      console.log("[ERROR] ", err);
+      res.destroy(500);
+    }
+  });
+  DATABASE_CONN.query("SELECT * FROM posty", (err, rows, fields) => {
+    if (err) {
+      console.log("[ERROR] ", err);
+      res.destroy(500);
+    }
+  });
+});
+
+app.listen(3000, "localhost", () => {
+  console.log("[START] SERVER LISTENING ON PORT 3000");
 });
