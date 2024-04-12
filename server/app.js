fc53e3090fe60da0d059cd6cebaa461abfa6f97d
KariminK <103779342+KariminK@users.noreply.github.com>
2024-04-12 21:15:04 +0200

    removed user

--- server/app.js
+++ server/app.js
@@ -10,15 +10,3 @@ app.use("/posts", postsRouter);
 app.listen(3000, "localhost", () => {
   console.log("[START] SERVER LISTENING ON PORT 3000");
 });
-
-class User {
-  constructor(d1, d2, d3, d4) {
-    this.name = d1;
-    this.surname = d2;
-    this.age = d3;
-    this.job = d4;
-  }
-  wypiszImie() {
-    console.log(this.name);
-  }
-}
